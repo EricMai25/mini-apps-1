0e6d2e401d308daa4341491b40fa3c52d5428f1b
Eric Mai <maieric25@gmail.com>
2018-12-10 19:34:48 -0800

    Players turn glow

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -8,15 +8,19 @@
 // document.getElementById('7').onClick(XO);
 // document.getElementById('8').onClick(XO);
 // document.getElementById('9').onClick(XO);
-
+var player1 = document.getElementsByClassName('player1')
+var player2 = document.getElementsByClassName('player2')
 var current = 'X'
 var rl = [];
 var lr = [];
+var xWin = [];
+var oWin = [];
 var inputArr= [0,0,0,0,0,0,0,0,0]
 var counter = 0
 var XO = (e)=>{
+    var turn = document.getElementsByClassName('turn')
     var haveWinner = document.getElementsByClassName('Winner')
-    var tr = document.getElementById(e.id)
+    var tr = document.getElementById(e.path[0].id)
     if(haveWinner[0].innerHTML){
         alert('Game Already Decided Please Reset')
         return
@@ -27,36 +31,56 @@ var XO = (e)=>{
         return
     }
 
-    if(['0','4','8'].includes(e.id)){
+    if(['0','4','8'].includes(e.path[0].id)){
         rl.push(current)
     }
-    if(['2','4','6'].includes(e.id)){
+    if(['2','4','6'].includes(e.path[0].id)){
         lr.push(current)
     }
     if(current === 'X'){
-        inputArr[e.id] = current
+        
+        inputArr[e.path[0].id] = current
         current = 'O'
+        player2[0].classList.toggle("glow")
+        player1[0].classList.toggle("glow")
+
         counter++
     }else {
-        inputArr[e.id] = current
+        inputArr[e.path[0].id] = current
+        player2[0].classList.toggle("glow")
+        player1[0].classList.toggle("glow")
+
         current = 'X'
         counter++
     }
-    console.log(rl , lr)
-    var matrix = matrixify(inputArr)
-    var colMatrix = spin(matrix)
+    const matrix = matrixify(inputArr)
+    const colMatrix = spin(matrix)
     winner(matrix)
     winner(colMatrix)
+    diagCheck()
+    tally(oWin,xWin)
+    turn[0].innerHTML= 'Turn: ' + current
+
 
  
 
 
 }
+var tally = (o , x)=>{
+    var talX = document.getElementsByClassName('tallyX')
+    var talO = document.getElementsByClassName('tallyO')
+
+    talX[0].innerHTML = talX[0].innerHTML += x.join('')
+    talO[0].innerHTML = talO[0].innerHTML += o.join('')
+
+}
 var clearOut= (id) =>{
    const ele =  document.getElementById(id)
    const emp = document.getElementsByTagName('th')
-   rl = [];
-   lr = [];
+    rl = [];
+    lr = [];
+    xWin = [];
+    oWin = [];
    inputArr= [0,0,0,0,0,0,0,0,0]
    counter= 0
    for(var i = 0; i < emp.length; i++){
@@ -93,33 +117,68 @@ var winner=(matrix)=>{
         if(array.every(v => v === 'X')){
             xWinner[0].innerHTML = " X is the Winner"
             current = 'O'    
+            xWin.push('l')
+            return
             
         }
         if(array.every(v => v === 'O')){
+
             xWinner[0].innerHTML = " O is the Winner"
             current = 'X' 
+            oWin.push('l')
+            return
+
         }
       })
-      if( rl.length === 3){
-          if(rl.every(v => v === 'X')){
+      
+}
 
-                  xWinner[0].innerHTML = " X is the Winner"
-                  current = 'O' 
-          }
-          if(rl.every(x => x === 'O')){
+var diagCheck = ()=>{
+    var xWinner = document.getElementsByClassName('Winner')
+    if( rl.length === 3){
+        if(rl.every(v => v === 'X')){
 
-                  xWinner[0].innerHTML = " O is the Winner"
-                  current = 'X' 
-          }
-      }
-      if( lr.length === 3){
-        if(lr.every(v => v === 'X')){
-                xWinner[0].innerHTML = " X is the Winner"
-                current = 'O' 
+              xWinner[0].innerHTML = " X is the Winner"
+              current = 'O' 
+              xWin.push('l')
+
         }
-        if(lr.every(x => x === 'O')){
-                xWinner[0].innerHTML = " O is the Winner"
-                current = 'X' 
+        if(rl.every(x => x === 'O')){
+
+              xWinner[0].innerHTML = " O is the Winner"
+              current = 'X' 
+              oWin.push('l')
+
         }
+        return
     }
+    if( lr.length === 3){
+      if(lr.every(v => v === 'X')){
+
+              xWinner[0].innerHTML = " X is the Winner"
+              current = 'O'
+              xWin.push('l')
+
+      }
+      if(lr.every(x => x === 'O')){
+              xWinner[0].innerHTML = " O is the Winner"
+              current = 'X' 
+              oWin.push('l')
+
+      }
+      return
+  }
+}
+
+var tester= (event)=>{
+    console.log(event)
+    console.log(event.path[0].id)
+}
+
+var player=()=>{
+    var play = prompt("Ready Player: ", 'One')
+    var play2= prompt("Ready Player: ", "Two")
+    player1[0].classList.toggle("glow")
+    player1[0].innerHTML = play
+    player2[0].innerHTML = play2
 }
